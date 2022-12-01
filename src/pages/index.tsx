a691d9149330c356a95675dae3d1b8d5268b4e3b
Sijad Khan <86185832+Sijadkhan1678@users.noreply.github.com>
2022-12-01 06:01:03 +0500

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,55 @@
 import * as React from "react"
 import type { HeadFC } from "gatsby"
 import Layout from "../components/Layout";
-import Todos from '../components/Todos'
+import { gql,useMutation } from '@apollo/client';
+import { Box,Fab,Typography,IconButton } from '@mui/material'
+import AddIcon from '@mui/icons-material/Add'
+import LollyForm from '../components/LollyForm'
+import { LollyData  } from '../interface/interfaces'
 
+const CREATE_LOLLY = gql`
 
+mutation createLolly($flavourTop:String!,$flavourMiddle:String!,$flavourBottom: String!,$recipient:String!,$message:String!,$sender:String!,$url:String!) {
+
+   createLolly ( flavourTop: $flavourTop,flavourMiddle: $flavourMiddle,
+flavourBottom : $flavourBottom,recipient : $recipient,message:  $message,sender: $sender,url: $url
+) {
+    id
+    flavourTop
+    flavourMiddle
+    flavourBottom
+    recipient
+    message
+    sender
+    url
+  }
+ }`
 
 const Home:React.FC = () => {
+
+ const [loading,setLoading] = useState < boolean > (false)
+  const [lollyGenerator,{ data }] = useMutation(CREATE_LOLLY);
+  
+const generateLolly = async (lolly: LollyData) => {
+    
+    setLoading(true)
+
+   await lollyGenerator({variables:lolly})
+   
+  // setTimeout( ()=> {
+     setLoading(false)
+  // },1000 )
+}
+     
+
   return (
     <Layout >
-    <Todos />
       
-      
+    <Box  color="white" py={4}>
+   <LollyForm generateLolly={generateLolly} loading={loading} />
+   { data && <LollyItem  lolly={data.createLolly}
+  /> }
+  </Box>
     </Layout>
   )
 }
